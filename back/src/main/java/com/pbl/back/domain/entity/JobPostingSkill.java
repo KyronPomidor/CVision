8a979f25dc100cbd6dcb037babb04e7d32f0602c
KyronPomidor <kirill.2xbo.c@gmail.com>
2026-09-03 18:00:28 +0300

    feat/general entities and enums created

--- back/src/main/java/com/pbl/back/domain/entity/JobPostingSkill.java
+++ back/src/main/java/com/pbl/back/domain/entity/JobPostingSkill.java
@@ -1,4 +1,31 @@
 package com.pbl.back.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(
+        name = "job_posting_skills",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"job_posting_id", "skill_id"})
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
 public class JobPostingSkill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "job_posting_id", nullable = false)
+    private JobPosting jobPosting;
+
+    @ManyToOne
+    @JoinColumn(name = "skill_id", nullable = false)
+    private Skill skill;
 }
--- back/src/main/java/com/pbl/back/domain/entity/User.java
+++ back/src/main/java/com/pbl/back/domain/entity/User.java
@@ -1,4 +1,38 @@
 package com.pbl.back.domain.entity;
 
+import com.pbl.back.domain.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "account_name", nullable = false)
+    private String accountName;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
 }
