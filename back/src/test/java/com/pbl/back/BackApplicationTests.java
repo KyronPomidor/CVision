2919cfff3793b732d398ef52bed91a4d65f8ab7d
KyronPomidor <kirill.2xbo.c@gmail.com>
2026-09-03 10:31:46 +0300

    Initial commit

---
+++ back/src/test/java/com/pbl/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbl.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
