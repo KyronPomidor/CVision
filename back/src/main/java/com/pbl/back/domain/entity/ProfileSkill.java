20d6f2e52d8156ffdfd4c44e491fbe5462f78f67
KyronPomidor <kirill.2xbo.c@gmail.com>
2026-09-03 18:00:28 +0300

    feat/general entities and enums created

--- back/src/main/java/com/pbl/back/domain/entity/ProfileSkill.java
+++ back/src/main/java/com/pbl/back/domain/entity/ProfileSkill.java
@@ -1,4 +1,35 @@
 package com.pbl.back.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(
+        name = "profile_skills",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"profile_id", "skill_id"})
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
 public class ProfileSkill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "profile_id", nullable = false)
+    private CandidateProfile profile;
+
+    @ManyToOne
+    @JoinColumn(name = "skill_id", nullable = false)
+    private Skill skill;
+
+    private String level;
+
+    private Integer yearsOfExperience;
 }
