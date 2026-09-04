f3027b4de0b8b244c5876dae20385a5356778c7a
KyronPomidor <kirill.2xbo.c@gmail.com>
2026-09-04 14:26:11 +0300

    feat: new candidateProfile flow
    # Conflicts:
    #	back/data/testdb.mv.db
    #	back/src/main/java/com/pbl/back/controller/CandidateProfileController.java
    #	back/src/main/java/com/pbl/back/mapper/CandidateProfileMapper.java
    #	back/src/main/java/com/pbl/back/mapper/impl/CandidateProfileMapperImpl.java
    #	back/src/main/java/com/pbl/back/service/CandidateProfileService.java
    #	back/src/main/java/com/pbl/back/service/impl/CandidateProfileServiceImpl.java

--- back/src/main/java/com/pbl/back/controller/CandidateProfileController.java
+++ back/src/main/java/com/pbl/back/controller/CandidateProfileController.java
@@ -1,23 +1,21 @@
 package com.pbl.back.controller;
 
-import com.pbl.back.dto.candidateprofile.CandidateProfileRequest;
-import com.pbl.back.dto.candidateprofile.CandidateProfileResponse;
+import com.pbl.back.dto.CandidateProfileRequest;
+import com.pbl.back.dto.CandidateProfileResponse;
 import com.pbl.back.service.CandidateProfileService;
-import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/profiles")
+@RequiredArgsConstructor
 public class CandidateProfileController {
 
     private final CandidateProfileService service;
 
-    public CandidateProfileController(CandidateProfileService service) {
-        this.service = service;
-    }
-
     @PostMapping
-    public CandidateProfileResponse create(@Valid @RequestBody CandidateProfileRequest request) {
+    public CandidateProfileResponse create(
+            @RequestBody CandidateProfileRequest request) {
 
         return service.create(request);
     }
