02dacec7a9332921a9518382453dd5345f70e4a2
KyronPomidor <kirill.2xbo.c@gmail.com>
2026-09-04 17:44:12 +0300

    feat: created user main classes

--- back/src/main/java/com/pbl/back/service/UserService.java
+++ back/src/main/java/com/pbl/back/service/UserService.java
@@ -5,10 +5,4 @@ import com.pbl.back.dto.user.UserResponse;
 
 public interface UserService {
     UserResponse create(UserRequest request);
-
-    UserResponse getById(Long id);
-
-    UserResponse update(Long id, UserRequest request);
-
-    void delete(Long id);
 }
