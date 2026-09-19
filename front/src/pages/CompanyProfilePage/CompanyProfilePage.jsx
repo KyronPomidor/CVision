c1c97d33f249a89b38f3737d2a8e270af30ca2e7
Makday <58140558+Makday@users.noreply.github.com>
2026-09-20 01:33:34 +0300

    add main layout

--- front/src/pages/CompanyProfilePage/CompanyProfilePage.jsx
+++ front/src/pages/CompanyProfilePage/CompanyProfilePage.jsx
@@ -1,19 +1,31 @@
-import styles from "./CompanyProfilePage.module.css"
+import styles from "./CompanyProfilePage.module.css";
+import CompanyHeaderCard from "./components/CompanyHeaderCard/CompanyHeaderCard";
+import AboutCard from "./components/AboutCard/AboutCard";
+import CompanyDetailsCard from "./components/CompanyDetailsCard/CompanyDetailsCard";
+import JobOffersCard from "./components/JobOffersCard/JobOffersCard";
+import GalleryCard from "./components/GalleryCard/GalleryCard";
 
 function CompanyProfilePage() {
-    return (
-      <div className={styles.CompanyProfilePage}>
-        <div>
-            <CompanyHeaderCard />
-            <AboutCard />
-            <GalleryCard />
-        </div>
-        <div>
-            <CompanyDetailsCard />
-            <JobOffersCard />
-        </div>
+  return (
+    <div className={styles.CompanyProfilePage}>
+      <div className={styles.column}>
+          <CompanyHeaderCard/>
+          <AboutCard/>
+          <GalleryCard/>
       </div>
-    )
-  }
+      <div className={styles.column}>
+          <CompanyDetailsCard 
+            socials={{
+              linkedin: "https://linkedin.com/company/roslin",
+              instagram: "https://instagram.com/roslin",
+              twitter: "https://twitter.com",
+              facebook: "https://facebook.com"
+              }}
+          />
+          <JobOffersCard/>
+      </div>
+    </div>
+  )
+}
   
-  export default CompanyProfilePage
+export default CompanyProfilePage
