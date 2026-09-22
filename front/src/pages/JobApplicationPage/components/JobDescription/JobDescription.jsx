7162eef0658ec15062e700816e08976618d433f0
Makday <58140558+Makday@users.noreply.github.com>
2026-09-22 23:03:25 +0300

    implement job description card

---
+++ front/src/pages/JobApplicationPage/components/JobDescription/JobDescription.jsx
@@ -0,0 +1,45 @@
+import styles from "./JobDescription.module.css";
+import typography from "../../../../Typography.module.css";
+import Star from "../../../../assets/star-logo.svg?react";
+
+import description from "./assets/yapping.txt?raw";
+
+const skills = [
+    "Golang",
+    "Kubernetes",
+    "Docker",
+    "gRPC",
+    "Kafka",
+    "PostgreSQL",
+]
+
+function JobDescription({ className='', ...props }) {
+  return (
+    <div className={`${className} ${styles.container}`} {...props}>
+      <h2 className={typography.heading2}>Job Description</h2>
+      <p className={`${styles.body} ${typography.mainText}`}>
+        {description}
+      </p>
+      <div className={styles.skillSection}>
+        <span className={`${typography.mainTextImportant} ${styles.subTitle}`}>
+            Necessary Skills (AI extracted)
+        </span>
+        <div className={styles.skillsRow}>
+            {skills.map((skill) => (
+            <span key={skill} className={`${typography.mainTextImportant} ${styles.skillPill}`}>
+                {skill}
+            </span>
+            ))}
+        </div>
+      </div>
+      <div className={styles.buttonRow}>
+        <button className={`${typography.heading2} ${styles.apply}`}>Apply</button>
+        <button className={styles.favorite}>
+            <Star className={styles.starLogo}/>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default JobDescription;
