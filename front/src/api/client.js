37452cb6272c3e7fadf56aa0ab5f0b6b7e9d60dd
Makday <58140558+Makday@users.noreply.github.com>
2026-09-26 17:56:40 +0300

    add expired token handling into client interceptor

--- front/src/api/client.js
+++ front/src/api/client.js
@@ -6,11 +6,29 @@ const apiClient = axios.create({
 });
 
 apiClient.interceptors.request.use((config) => {
+  const isAuthRoute = config.url?.startsWith("/auth") || config.url === "/users";
   const token = localStorage.getItem("token");
-  if (token) {
+  if (token && !isAuthRoute) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if(error.response?.status === 401) {
+      logout();
+    }
+    return Promise.reject(error);
+  }
+);
+
+function logout() {
+  localStorage.removeItem("token");
+  if (window.location.pathname !== "/login") {
+    window.location.href = "/login";
+  }
+}
 
 export default apiClient;
