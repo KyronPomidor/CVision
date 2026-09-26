f4841373b9e67889f6aa1c8296668a7abea64da3
Makday <58140558+Makday@users.noreply.github.com>
2026-09-26 18:16:19 +0300

    format code using prettier

--- front/src/api/client.js
+++ front/src/api/client.js
@@ -17,11 +17,11 @@ apiClient.interceptors.request.use((config) => {
 apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
-    if(error.response?.status === 401) {
+    if (error.response?.status === 401) {
       logout();
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 function logout() {
--- front/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ front/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,14 +1,14 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 function ProtectedRoute() {
-    const token = localStorage.getItem("token");
-    const location = useLocation();
+  const token = localStorage.getItem("token");
+  const location = useLocation();
 
-    if (!token) {
-        return <Navigate to="/login" state={{ from: location }} replace />
-    }
+  if (!token) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 
-    return <Outlet />;
+  return <Outlet />;
 }
 
 export default ProtectedRoute;
